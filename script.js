60eb9174092d8bec8818bbbe3fc6c494d5552614
Shashank Naik <shashanknaik808@gmail.com>
2023-10-17 14:39:39 +0530

    Defining the 'play()' method

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const guessedNumsRef = document.getElementById("guessed-nums");
 const restartButton = document.getElementById("restart");
 const game = document.getElementById("game");
 const guessInput = document.getElementById("guess");
+const checkButton = document.getElementById("check-btn");
 
 
 let answer, noOfGuesses, guessedNumsArr;
 
+
+const play = () => {
+    console.log("called the play");
+};
+
+
+checkButton.addEventListener("click", play);
