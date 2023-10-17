a40ae056f6f01b1a978e7f1a9d94a800cf300160
Shashank Naik <shashanknaik808@gmail.com>
2023-10-17 15:14:59 +0530

    Added feedback and game completion logic

--- script.js
+++ script.js
@@ -13,13 +13,34 @@ let answer, noOfGuesses, guessedNumsArr;
 const play = () => {
     const userGuess = guessInput.value;
     if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
-      alert("Please enter a valid number between 1 and 100.");
-      return;
+        alert("Please enter a valid number between 1 and 100.");
+        return;
     }
     guessedNumsArr.push(userGuess);
     noOfGuesses += 1;
-    
-  };
+
+    if (userGuess != answer) {
+        if (userGuess < answer) {
+            hint.innerHTML = "Too low. Try Again!";
+        } else {
+            hint.innerHTML = "Too high. Try Again!";
+        }
+        noOfGuessesRef.innerHTML = `<span>No. Of Guesses:</span> ${noOfGuesses}`;
+        guessedNumsRef.innerHTML = `<span>Guessed Numbers are: </span>${guessedNumsArr.join(
+            ","
+        )}`;
+        hint.classList.remove("error");
+        setTimeout(() => {
+            hint.classList.add("error");
+        }, 10);
+    } else {
+        hint.innerHTML = `Congratulations!<br>The number was <span>${answer}</span>.<br>You guessed the number in <span>${noOfGuesses} </span>tries.`;
+        hint.classList.add("success");
+        game.style.display = "none";
+        restartButton.style.display = "block";
+    }
+
+};
 
 
 checkButton.addEventListener("click", play);
