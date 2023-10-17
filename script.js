37045486b51c7f8247b5bb0955e2f642c8c68fce
Shashank Naik <shashanknaik808@gmail.com>
2023-10-17 14:34:22 +0530

    Declaring the Initial Reference Variables

---
+++ script.js
@@ -0,0 +1,10 @@
+const hint = document.getElementById("hint");
+const noOfGuessesRef = document.getElementById("no-of-guesses");
+const guessedNumsRef = document.getElementById("guessed-nums");
+const restartButton = document.getElementById("restart");
+const game = document.getElementById("game");
+const guessInput = document.getElementById("guess");
+
+
+let answer, noOfGuesses, guessedNumsArr;
+
